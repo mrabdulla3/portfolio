de0e94d1e594a47cd927abeb9a7c58a8fc6c9ead
Mohd Abdulla <abdullagaur6559@gmail.com>
2025-01-31 22:54:56 +0530

    User Interface updated

--- portfolio/port.js
+++ portfolio/port.js
@@ -1,4 +1,3 @@
-console.log("script running...")
 document.querySelector('.cross').style.display='none';
 document.querySelector('.hamburger').addEventListener("click",()=>{
 document.querySelector('.sidebar').classList.toggle('sidebarGo');
@@ -10,4 +9,27 @@ else{
     document.querySelector('.ham').style.display='none';
     document.querySelector('.cross').style.display='inline';
 }
+});
+
+document.getElementById('downloadAgri').addEventListener('click', function () {
+    window.open(
+        "https://docs.google.com/uc?export=download&id=1F9eb7Y8E-vaSvmpiBggU3xrX42pzLxAA", 
+        "_blank", 
+        "noopener,noreferrer"
+    );
+});
+
+document.getElementById('downloadGemini').addEventListener('click', function () {
+    window.open(
+        "https://docs.google.com/uc?export=download&id=1c0Yd5bo0aujyrkcPyYYVHoIr7T8eYKFg", 
+        "_blank", 
+        "noopener,noreferrer"
+    );
+});
+document.getElementById('downloadHacker').addEventListener('click', function () {
+    window.open(
+        "https://docs.google.com/uc?export=download&id=1cj2IMEc7nmJQuzAMwWHT4K_UmoVFmA-y", 
+        "_blank", 
+        "noopener,noreferrer"
+    );
 });
